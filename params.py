e2ae831b3e5ff7819269bae2dd3ecba212933139
Chris Jarrett <chris.jarrett@acurable.com>
2023-02-27 21:33:24 +0000

    Uploaded images to cloud

---
+++ params.py
@@ -0,0 +1,5 @@
+WANDB_PROJECT = "mlops-course-assignment"
+ENTITY = None # set this to team name if working in a team
+RAW_DATA_AT = 'raw_data_at'
+PROCESSED_DATA_AT = 'processed_data_at'
+CLASSES = {"healthy": 0, "tumor":1}
